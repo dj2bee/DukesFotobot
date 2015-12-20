1f8d2e6797c1997a508a05ea265ef3cf90f4e27b
dj2bee <kontakt@tobias-pohlmann.com>
2015-12-20 18:09:30 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var fs = require('fs'),
+	telegramBot = require('node-telegram-bot-api'),
+	token = fs.readFileSync('./config/token', 'utf8'),
+	bot = new telegramBot(token, {polling: true});
+
+bot.on('message', function(msg) {
+	var text = msg.text;
+	if(text[0] == '/') {
+		var command = text.split(' ');
+		switch(command[0]) {
+			case '/sitz':
+				bot.sendMessage(msg.chat.id, 'Okay, ich sitze!');
+				break;
+			case '/down':
+				bot.sendMessage(msg.chat.id, 'Bäh! Der Boden ist kalt... Bekomme ich wenigstens einen Keks?');
+				break;
+			case '/touch':
+				bot.sendMessage(msg.chat.id, 'Patsch! Kalte Nase trifft duftende Hand <3');
+				break;
+			default:
+				bot.sendMessage(msg.chat.id, 'Das Kommando kenne ich noch nicht.');
+		}
+	}
+	console.log(msg.from.first_name + ' (' + msg.from.username + ')' + ': ' + msg.text);
+});
+
+console.log('DukesFotoBot wach, gefüttert und schnüffelbereit!\n');
